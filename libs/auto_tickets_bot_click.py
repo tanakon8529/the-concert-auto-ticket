c418145d1eb288c9cee0534664538cc5ea00b1f2
Tanakon Kabprapun <tanakon_ka@dtgo.com>
2022-06-21 23:22:41 +0700

    it work! : but can't select zone, bot will choice pointer_button on top and can't select seat (auto random).

--- libs/auto_tickets_bot_click.py
+++ libs/auto_tickets_bot_click.py
@@ -1,3 +1,4 @@
+import re
 from utils.browser_uill import browser_controls
 from utils.auto_click_utill import auto_click_controls
 
@@ -11,9 +12,13 @@ class auto_tickets_controls(object):
 
     def open_and_click_get_ticket(self, url_get_ticket, pic_get_ticket):
         status_click_button = False
+        count_round = 0
         self.browser.open_browser(url_get_ticket)
         while status_click_button != True:
+            if (count_round % 2 == 0):
+                self.auto_click.funtion_five()
             status_click_button = self.auto_click.once_click_button(pic_get_ticket)
+            count_round += 1
 
     def copy_url_and_process_stage_page(self, pic_url_stage):
         status_click_button = False
@@ -25,18 +30,35 @@ class auto_tickets_controls(object):
             status_page_stage = self.browser.check_stage_page(url_stage)
         return status_page_stage
 
-    def click_pointer_and_add_tickets(self, pic_pointer_button, pic_add_tickets, total_tickets, pic_buy_tickeys):
+    def click_pointer_and_add_tickets(self, pic_pointer_button, pic_seat_button, pic_sold_out_button, pic_add_tickets, total_tickets, pic_buy_tickeys):
         status_click_pointer_button = False
         status_click_add_button = False
         status_click_buy_button = False
+        status_click_seat_button = False
+        count_scroll_down = 0
 
         while status_click_pointer_button != True:
             status_click_pointer_button = self.auto_click.once_click_button(pic_pointer_button)
 
-        for total in range(total_tickets-1):
-            status_click_add_button = self.auto_click.once_click_button(pic_add_tickets)
+            if status_click_pointer_button == True:
+                while status_click_seat_button != True:
+                    for total in range(total_tickets-1):
+                        status_click_seat_button = self.auto_click.once_click_button_05(pic_seat_button)
 
-        if status_click_add_button == True:
+            if status_click_pointer_button == False:
+                self.auto_click.once_click_button(pic_sold_out_button)
+                self.auto_click.scroll_down()
+                count_scroll_down += 1
+                logger.debug("round scroll : {}".format(count_scroll_down))
+
+                if count_scroll_down > 3:
+                    return status_click_buy_button
+
+        if status_click_seat_button == False:
+            for total in range(total_tickets-1):
+                status_click_add_button = self.auto_click.once_click_button(pic_add_tickets)
+
+        if status_click_add_button == True or status_click_seat_button == True:
             status_click_buy_button = self.auto_click.once_click_button(pic_buy_tickeys)
 
         return status_click_buy_button
@@ -47,13 +69,20 @@ class auto_tickets_controls(object):
         pic_get_ticket = payload["pic_get_ticket"]
         pic_url_stage = payload["pic_url_stage"]
         pic_pointer_button = payload["pic_pointer_button"]
+        pic_seat_button = payload["pic_seat_button"]
+        pic_sold_out_button = payload["pic_sold_out_button"]
         pic_add_tickets = payload["pic_add_tickets"]
         total_tickets = payload["total_tickets"]
         pic_buy_tickeys = payload["pic_buy_tickeys"]
 
-        self.open_and_click_get_ticket(url_get_ticket, pic_get_ticket)
-        status_page_stage = self.copy_url_and_process_stage_page(pic_url_stage)
-        if status_page_stage:
-            status_click_buy_button = self.click_pointer_and_add_tickets(pic_pointer_button, pic_add_tickets, total_tickets, pic_buy_tickeys)
-            logger.debug("ready to but tickets : {}".format(status_click_buy_button))
+        status_click_buy_button = False
+
+        while status_click_buy_button != True:
+            if status_click_buy_button == False:
+                self.open_and_click_get_ticket(url_get_ticket, pic_get_ticket)
+
+            status_page_stage = self.copy_url_and_process_stage_page(pic_url_stage)
+            if status_page_stage:
+                status_click_buy_button = self.click_pointer_and_add_tickets(pic_pointer_button, pic_seat_button, pic_sold_out_button, pic_add_tickets, total_tickets, pic_buy_tickeys)
+                logger.debug("ready to buy tickets : {}".format(status_click_buy_button))
 
--- utils/auto_click_utill.py
+++ utils/auto_click_utill.py
@@ -23,7 +23,8 @@ class auto_click_controls(object):
     def once_click_button(self, path_pic):
         status_click_button = False
         try:
-            start = time() + 5
+            start = time() + 1
+            count_round = 0
             while time() < start:
                 button_join = pyautogui.locateOnScreen(self.real_path_resource+'/'+path_pic  , confidence=0.7)
                 if button_join:
@@ -31,6 +32,29 @@ class auto_click_controls(object):
                     status_click_button = True
                     logger.debug("click button : {}".format(status_click_button))
                     break
+                
+                count_round += 1
+                logger.debug("count click button : {}".format(count_round))
+
+            return status_click_button
+        except Exception as e:
+            logger.error("{}".format(e)) 
+
+    def once_click_button_05(self, path_pic):
+        status_click_button = False
+        try:
+            start = time() + 1
+            count_round = 0
+            while time() < start:
+                button_join = pyautogui.locateOnScreen(self.real_path_resource+'/'+path_pic  , confidence=0.4)
+                if button_join:
+                    pyautogui.click(button_join)
+                    status_click_button = True
+                    logger.debug("click button : {}".format(status_click_button))
+                    break
+                
+                count_round += 1
+                logger.debug("count click button : {}".format(count_round))
 
             return status_click_button
         except Exception as e:
@@ -54,3 +78,12 @@ class auto_click_controls(object):
 
     def ctrl_v(self, path_pic):
         pyautogui.hotkey('ctrl', 'v')
+
+    def funtion_five(self):
+        pyautogui.press('f5')
+        logger.debug("Refresh !")
+
+    def scroll_down(self):
+        for scroll_scale in range(5):
+            pyautogui.scroll(-1)
+
